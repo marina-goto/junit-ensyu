26a14a82dfc4987570b32a58908edc2f48058120
marina goto <mari@atofujimarinanoMacBook-Air.local>
2022-08-15 16:31:12 +0900

    ex2-1

---
+++ src/main/java/junit/tutorial/ex02/e01/test.java
@@ -0,0 +1,9 @@
+package junit.tutorial.ex02.e01;
+
+public class test {
+	public static void main(String[] args) {
+		FizzBuzz fizzBuzz=new FizzBuzz();
+		System.out.println(fizzBuzz.createFizzBuzzList(16));
+	}
+
+}
--- src/test/java/junit/tutorial/ex02/e01/FizzBuzzTest.java
+++ src/test/java/junit/tutorial/ex02/e01/FizzBuzzTest.java
@@ -2,6 +2,9 @@ package junit.tutorial.ex02.e01;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -28,7 +31,27 @@ class FizzBuzzTest {
 
 	@Test
 	void testCreateFizzBuzzList() {
-		fail("Not yet implemented");
+		FizzBuzz fizzBuzz=new FizzBuzz();
+		List<String>answer=fizzBuzz.createFizzBuzzList(16);
+		List<String>test=new ArrayList<>();
+		test.add("1");
+		test.add("2");
+		test.add("Fizz");
+		test.add("4");
+		test.add("Buzz");
+		test.add("Fizz");
+		test.add("7");
+		test.add("8");
+		test.add("Fizz");
+		test.add("Buzz");
+		test.add("11");
+		test.add("Fizz");
+		test.add("13");
+		test.add("14");
+		test.add("FizzBuzz");
+		test.add("16");
+		
+		assertIterableEquals(test,answer,"違うよ");
 	}
 
 }
