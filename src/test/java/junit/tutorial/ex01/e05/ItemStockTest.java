69e75b6355665193a96bc69fead8f9bd23f1a246
marina goto <mari@atofujimarinanoMacBook-Air.local>
2022-08-15 16:02:49 +0900

    ex1-5

--- src/test/java/junit/tutorial/ex01/e05/ItemStockTest.java
+++ src/test/java/junit/tutorial/ex01/e05/ItemStockTest.java
@@ -8,6 +8,8 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import junit.tutorial.ex01.e04.NumberUtils;
+
 class ItemStockTest {
 
 	@BeforeAll
@@ -27,13 +29,49 @@ class ItemStockTest {
 	}
 
 	@Test
-	void testGetNum() {
-		fail("Not yet implemented");
+	void testGetNum1() {
+		ItemStock itemStock = new ItemStock();
+		int answer = itemStock.getNum(new Item("book", 100));
+		assertEquals(0, answer, "違うよ");
 	}
 
 	@Test
-	void testAdd() {
-		fail("Not yet implemented");
+	void testGetNum2() {
+		Item item = new Item("book", 100);
+		ItemStock itemStock = new ItemStock();
+		itemStock.add(item);
+		int answer = itemStock.getNum(item);
+		assertEquals(1, answer, "違うよ");
 	}
 
+	void add1() {
+		Item item = new Item("book", 100);
+		ItemStock itemStock = new ItemStock();
+		itemStock.add(item);
+	}
+
+	@Test
+	void testGetNum3() {
+	}
+
+	@Test
+	void testGetNum4() {
+		Item item = new Item("book", 100);
+		ItemStock itemStock = new ItemStock();
+		itemStock.add(item);
+		itemStock.add(item);
+		int answer = itemStock.getNum(item);
+		assertEquals(2, answer, "違うよ");
+	}
+
+	@Test
+	void testGetNum5() {
+		Item item = new Item("book", 100);
+		ItemStock itemStock = new ItemStock();
+		itemStock.add(item);
+		Item cd = new Item("CD", 1000);
+		itemStock.add(cd);
+		int answer = itemStock.getNum(item);
+		assertEquals(1, answer, "違うよ");
+	}
 }
