bd31b03822f4e6845f02df9989902c0ab63c811e
marina goto <mari@atofujimarinanoMacBook-Air.local>
2022-08-15 12:38:29 +0900

    first commit

---
+++ src/test/java/junit/tutorial/ex01/e01/StringUtilsTest.java
@@ -0,0 +1,37 @@
+package junit.tutorial.ex01.e01;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import ex01.StringUtils;
+
+class StringUtilsTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testToSnakeCase() {
+		String snake=StringUtils.toSnakeCase("aaa");
+		assertEquals("aaa", snake,"違う");
+		}
+
+}
